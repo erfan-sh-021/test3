56d5fdeccd21088de130208e19988f449b4f5e93
erfan Shaghaghi <erfandc.es@gmail.com>
2022-12-19 00:17:46 +0330

    add  basic sass file and add descktop menu

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Layout from './component/layout/layout';
+import './scss/_main.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <>
+      <Layout/>
+    </>
+  );
+}
+
+export default App;
--- src/component/layout/layout.jsx
+++ src/component/layout/layout.jsx
@@ -1,9 +1,20 @@
-import '../css/layout.css';
+import '../../assets/fonts/icomoon/style.css';
+
 const Layout = () => {
     return ( 
-        <>
-
-        </>
+        <div className="main">
+            <div className="sideBar">
+                <div className="menuLogo"><span className="icon-Pardik"></span></div>
+                <ul className="items">
+                    <li>صفحه نخست<span className="icon-PardikHouse"></span></li>
+                    <li>پیام ها<span className="icon-announcement"></span></li>
+                    <li>رزرو سرویس<span className="icon-reservation"></span></li>
+                    <li>پرداخت شارژ/بدهی<span className="icon-announcement"></span></li>
+                    <li>لیست تراکنش ها<span className="icon-invoice"></span></li>
+                    <li>خروج<span className="icon-exit"></span></li>
+                </ul>
+            </div>
+        </div>
      );
 }
  
