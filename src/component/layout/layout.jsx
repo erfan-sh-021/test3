0978b9679bb960273ac09e900f302bc4b164500a
erfan Shaghaghi <erfandc.es@gmail.com>
2022-12-20 17:53:42 +0330

    fix parent div and

--- src/component/layout/layout.jsx
+++ src/component/layout/layout.jsx
@@ -11,48 +11,50 @@ import '../../assets/fonts/icomoon/style.css';
 // })
 // console.log(iconMenu)
 const Layout = () => {
-    useEffect(()=>{
+    useEffect(() => {
         const iconMenu = document.querySelector('.icon-menu');
         const iconMenuCloseX = document.querySelector('.icon-closeX');
-        const menuItems =document.querySelector('.menuItems');
-        const main =document.querySelector('.main');
+        const menuItems = document.querySelector('.menuItems');
+        const menu = document.querySelector('.menu');
         iconMenu.addEventListener('touchend', () => {
-            main.classList.remove('background')
+            menu.classList.remove('background')
             menuItems.classList.remove('deactive');
             iconMenu.classList.add('deactive');
             iconMenuCloseX.classList.remove('deactive');
         })
         iconMenuCloseX.addEventListener('touchend', () => {
-            main.classList.add('background')
+            menu.classList.add('background')
             menuItems.classList.add('deactive');
             iconMenuCloseX.classList.add('deactive');
             iconMenu.classList.remove('deactive');
         })
 
-        
-    },[])
+
+    }, [])
     return (
-        <div className="main background">
-            <div className="navbarMenu"><span className="icon-menu"/><span className="icon-closeX deactive"/ ></div>
-            <div className="menuItems deactive">
-                <div className="menuLogo">
-                    <span className="icon-Pardik" />
-                </div>
-                <div className="user">
-                    <div className="userDetails">
-                        <h4>فلان بهمانی</h4>
-                        <p>واحد 217</p>
+        <div className="page">
+            <div className="menu background">
+                <div className="navbarMenu"><span className="icon-menu" /><span className="icon-closeX deactive" /></div>
+                <div className="menuItems deactive">
+                    <div className="menuLogo">
+                        <span className="icon-Pardik" />
                     </div>
-                    <span className="icon-user" />
+                    <div className="user">
+                        <div className="userDetails">
+                            <h4>فلان بهمانی</h4>
+                            <p>واحد 217</p>
+                        </div>
+                        <span className="icon-user" />
+                    </div>
+                    <ul className="items">
+                        <li>صفحه نخست<span className="icon-PardikHouse" /></li>
+                        <li>پیام ها<span className="icon-announcement" /></li>
+                        <li>رزرو سرویس<span className="icon-reservation" /></li>
+                        <li>پرداخت شارژ/بدهی<span className="icon-announcement" /></li>
+                        <li>لیست تراکنش ها<span className="icon-invoice" /></li>
+                        <li>خروج<span className="icon-exit" /></li>
+                    </ul>
                 </div>
-                <ul className="items">
-                    <li>صفحه نخست<span className="icon-PardikHouse" /></li>
-                    <li>پیام ها<span className="icon-announcement" /></li>
-                    <li>رزرو سرویس<span className="icon-reservation" /></li>
-                    <li>پرداخت شارژ/بدهی<span className="icon-announcement" /></li>
-                    <li>لیست تراکنش ها<span className="icon-invoice" /></li>
-                    <li>خروج<span className="icon-exit" /></li>
-                </ul>
             </div>
         </div>
     );
