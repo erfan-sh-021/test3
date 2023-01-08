ddb4d30e2752835a3dbe2415c767764e54df6073
erfan Shaghaghi <erfandc.es@gmail.com>
2023-01-08 07:27:54 +0330

    fix reservation Date style

--- src/component/layout/layout.jsx
+++ src/component/layout/layout.jsx
@@ -13,8 +13,10 @@ import '../../assets/fonts/icomoon/style.css';
 // console.log(iconMenu)
 const Layout = () => {
     const menu = document.querySelector('.menu');
+    // const left = document.querySelector('.left');
     const [state, setState] = useState({ falseactiveFilter: 0 });
     const [click, setClick] = useState(false);
+    const [liClick, setLiClick] = useState(false);
     const [mobileWidth, setMobileWidth] = useState(false);
     const [width, setWidth] = useState(window.innerWidth);
     useEffect(() => {
@@ -23,9 +25,9 @@ const Layout = () => {
         const menuItems = document.querySelector('.menuItems');
         const menu = document.querySelector('.menu');
         const reservtion = document.querySelector('.reservtion');
-        const li = document.querySelector('.li');
+        // const li = document.querySelector('.li');
         iconMenu.addEventListener('click', () => {
-            console.log('clicked')
+            // console.log('clicked')
             menu.classList.remove('background')
             menuItems.classList.remove('deactive');
             iconMenu.classList.add('deactive');
@@ -43,7 +45,11 @@ const Layout = () => {
             iconMenu.classList.remove('deactive');
             setClick(true)
         })
-
+        // li.addEventListener(('click',(e)=>{
+        //     // setLiClick(true);
+        //     console.log('click');
+        // }))
+        // console.log(li)
         if (window.innerWidth < 769) {
             setMobileWidth(true);
             
@@ -57,6 +63,9 @@ const Layout = () => {
     if(click && mobileWidth === true){
         menu.classList.add('deactive')
     }
+    // if(liClick && mobileWidth === true){
+    //     left.classList.add('deactive')
+    // }
     // ---------------for active color in menu------------------
     const filter = (e, filterId) => {
         setState({
@@ -83,14 +92,14 @@ const Layout = () => {
                     </div>
                     <ul className="items">
                         <Link to={'/'}>
-                            <li className={`${state.activeFilter === 1 ? 'selected' : ''}`} onClick={(e) => filter(e, 1)}>صفحه نخست<span className="icon-PardikHouse" /></li>
+                            <li className={`${state.activeFilter === 1 ? 'selected' : ''} li`} onClick={(e) => filter(e, 1)}>صفحه نخست<span className="icon-PardikHouse" /></li>
                         </Link>
-                        <li className={`${state.activeFilter === 2 ? 'selected' : ''}`} onClick={(e) => filter(e, 2)} >پیام ها<span className="icon-announcement" /></li>
+                        <li className={`${state.activeFilter === 2 ? 'selected' : ''} li`} onClick={(e) => filter(e, 2)} >پیام ها<span className="icon-announcement" /></li>
                         <Link to={'/resevtion'}>
                             <li className={`${state.activeFilter === 3 ? 'selected' : ''} li reservtion`} onClick={(e) => filter(e, 3)} >رزرو سرویس<span className="icon-reservation" /></li>
                         </Link>
-                        <li className={`${state.activeFilter === 4 ? 'selected' : ''}`} onClick={(e) => filter(e, 4)}>پرداخت شارژ/بدهی<span className="icon-payment" /></li>
-                        <li className={`${state.activeFilter === 5 ? 'selected' : ''}`} onClick={(e) => filter(e, 5)}>لیست تراکنش ها<span className="icon-bill" /></li>
+                        <li className={`${state.activeFilter === 4 ? 'selected' : ''} li`} onClick={(e) => filter(e, 4)}>پرداخت شارژ/بدهی<span className="icon-payment" /></li>
+                        <li className={`${state.activeFilter === 5 ? 'selected' : ''} li`} onClick={(e) => filter(e, 5)}>لیست تراکنش ها<span className="icon-bill" /></li>
                         <li className={`${state.activeFilter === 6 ? 'selected' : ''} li exit`} onClick={(e) => filter(e, 6)} >خروج<span className="icon-exit" /></li>
                     </ul>
                 </div>
