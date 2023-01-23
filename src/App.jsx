a8e32a87735f1054371f247eaff47e8d3d8f22e4
erfan-sh-021 <erfandc.es@gmail.com>
2023-01-23 23:13:37 +0330

    *

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ const url ='https://pbmapi.pardikiot.com/api/ServiceTypes/GetMirrorServiceTypes'
 const config = {
   headers: { 
     'Accept': 'text/plain', 
-    'Authorization': 'Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIzMjhiNGI5Ni05YjY2LTRmZWUtOTA1ZC1hZmIwMWM0OWJhMGUiLCJ1bmlxdWVfbmFtZSI6IjExMDEiLCJVc2VyVHlwZSI6IjIiLCJSZXNpZGVudFVzZXJQYXJlbnRJZCI6ImM2ZjBkYjcxLWEwY2UtNGJmMC05OTBhLTgxMDZhNWMzYzYzMSIsIm5iZiI6MTY3Mzg2MjkzMywiZXhwIjoxNjc0NDY3NzMzLCJpYXQiOjE2NzM4NjI5MzN9.-1E8ADUElyI7sUtwyyX6M6E4jeEOMcxPEN1c0zRMt_vywpQCAarUi0kLD5Z8F-7w9Lf7-vARutRDNfzUd2Qdog'
+    'Authorization': 'Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIzMjhiNGI5Ni05YjY2LTRmZWUtOTA1ZC1hZmIwMWM0OWJhMGUiLCJ1bmlxdWVfbmFtZSI6IjExMDEiLCJVc2VyVHlwZSI6IjIiLCJSZXNpZGVudFVzZXJQYXJlbnRJZCI6ImM2ZjBkYjcxLWEwY2UtNGJmMC05OTBhLTgxMDZhNWMzYzYzMSIsIm5iZiI6MTY3NDQ5Nzk5MiwiZXhwIjoxNjc1MTAyNzkyLCJpYXQiOjE2NzQ0OTc5OTJ9.ioukubv_ciUSarn1_8jCBblDvCU7o6bNpHEbZ74E7rnuRLNMBsUs_RO2gSSNXj0OysKVlhdAc7Hyoj5ap1zTMQ'
   }
 }
 useEffect(()=>{
